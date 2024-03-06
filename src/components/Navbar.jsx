66d388bb6c61e6e5b3e3c7e498005be5a07b37a5
kirtanchandak <themarketingkirtan@gmail.com>
2024-03-06 12:20:58 +0530

    navbar and basic setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import logo from "../assets/logo.svg";
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between px-12 py-4 bg-white">
+      <div>
+        <img src={logo} className="w-28 text-[#F65858] mt-1" />
+      </div>
+      <div className="flex text-xl gap-8">
+        <div className="flex gap-8 mt-2 font-medium">
+          <p>Crypto Taxes</p>
+          <p>Free Tools</p>
+          <p>Resource Center</p>
+        </div>
+        <button className="py-2 px-3 bg-[#0052FE] text-white rounded-lg font-medium">
+          Get Started
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
