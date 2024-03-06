b236b26775e61d7731a410026821e81fbe696cf1
kirtanchandak <themarketingkirtan@gmail.com>
2024-03-06 15:48:36 +0530

    performance section complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,7 @@ import Navbar from "../components/Navbar";
 import axios from "axios";
 import bitcoin from "../assets/bitcoin.webp";
 import TradingViewWidget from "../components/TradingViewWidget";
-import redLine from "../assets/redline.svg";
-import smallRedLine from "../assets/smallRedLine.svg";
+import Performance from "../components/Home/Performance";
 
 function Home() {
   //   useEffect(() => {
@@ -38,41 +37,10 @@ function Home() {
     },
   ];
 
-  const labels = [
-    {
-      id: 1,
-      name: "Overview",
-    },
-    {
-      id: 2,
-      name: "Fundamentals",
-    },
-    {
-      id: 3,
-      name: "News Insights",
-    },
-    {
-      id: 4,
-      name: "Sentiments",
-    },
-    {
-      id: 5,
-      name: "Team",
-    },
-    {
-      id: 6,
-      name: "Technicals",
-    },
-    {
-      id: 7,
-      name: "Tokonomics",
-    },
-  ];
-
   return (
     <>
       <Navbar />
-      <div className="md:px-20 px-8 mt-8">
+      <div className="md:px-20 px-6 mt-8">
         <div>
           <p>Cryptocurrencies Bitcoin</p>
         </div>
@@ -127,55 +95,7 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div className="mt-5 overflow-hidden">
-              <div className="flex gap-8 py-2">
-                {labels.map((label) => (
-                  <p key={label.id}>{label.name}</p>
-                ))}
-              </div>
-              <hr />
-              <div className="bg-white md:py-6 py-1 px-6 rounded-lg mt-5">
-                <div className="flex">
-                  <h1 className="text-2xl font-bold mt-1">Performance</h1>
-                </div>
-                <div className="mt-6">
-                  <div className="flex md:gap-8 gap-4">
-                    <div className="">
-                      <p className="font-normal md:text-sm text-[#44475B] text-xs">
-                        Today's High
-                      </p>
-                      <p className="">49,343.83</p>
-                    </div>
-                    <img src={redLine} alt="" className="md:flex hidden" />
-                    <img src={smallRedLine} alt="" className="w-20 md:hidden" />
-                    <div className="">
-                      <p className="font-normal md:text-sm text-[#44475B] text-xs">
-                        Today's High
-                      </p>
-                      <p className="">49,343.83</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="mt-6">
-                  <div className="flex md:gap-8 gap-3">
-                    <div className="">
-                      <p className="font-normal text-sm text-[#44475B]">
-                        Today's Low
-                      </p>
-                      <p>46,930.22</p>
-                    </div>
-                    <img src={redLine} alt="" className="md:flex hidden" />
-                    <img src={smallRedLine} alt="" className="w-20 md:hidden" />
-                    <div className="">
-                      <p className="font-normal text-sm text-[#44475B]">
-                        Today's High
-                      </p>
-                      <p>49,343.83</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Performance />
           </div>
           <div>ok</div>
         </div>
