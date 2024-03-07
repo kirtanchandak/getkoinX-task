40c47a21f949856b3614276c2bf71577615179a1
kirtanchandak <themarketingkirtan@gmail.com>
2024-03-07 18:06:51 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const isFirstTime = localStorage.getItem("isFirstTime");
-
-    if (!isFirstTime) {
-      navigate("/bitcoin");
-      localStorage.setItem("isFirstTime", "false");
-    }
-  }, [navigate]);
-
   return (
     <>
       <Routes>
